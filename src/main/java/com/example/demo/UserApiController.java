eeb8b2ecb08c65fc8367c04ca75a97b29660f345
ganondorofu <ganondorofu3143@outlook.com>
2024-09-21 23:14:18 +0900

    むりぽ...

---
+++ src/main/java/com/example/demo/UserApiController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// ビジネスロックの処理するサービスのコントローラー
+@RestController 
+@RequestMapping("/api") // ベースのリンク/apiに指定
+public class UserApiController.java {
+
+    private final CommentService commentService;
+
+// commentServiceコントローラのコンストラクタさん
+    @Autowired 
+    public CommentApiController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+// postリクエストが来たら commentオブジェクト呼び出し
+    @PostMapping("/add-comment")
+    public void addComment(@RequestBody Comment comment) {
+    	UserService.saveComment(comment); // commentServiceのserveComment呼び出し受け取りデータの登録
+    }
+
+    @PostMapping("/api/select/{id}")
+    public void deleteCommentFromApi(@PathVariable Long id) {
+    	UserService.deleteComment(id);
+    }
+}
---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// モデル部分
+// JPAリポジトリ使ってSQL書かないで済んだからsecurity面で安心安全だと思う
+// Spring data JPAのインターフェース使用
+// エンティティクラス=Comment comment.javaが本体
+// ID= Long
+public interface UserRepository extends JpaRepository<Comment, Long> {
+}
